5ff84ae79fc823a554f8b4a403c0b0778b4387ac
sreeharipullasetty <33320657+sreeharipullasetty@users.noreply.github.com>
2019-11-05 23:51:51 +0530

    Add files via upload

--- src/main/webapp/scripts.js
+++ src/main/webapp/scripts.js
@@ -1,6 +1,4 @@
-function changeVideo(){
-	start=106
-	end=238
+function javaContent(start, end, subtopic){
+document.getElementById('subTopic').innerHTML = subtopic; 	
 document.getElementById('link').src = "https://www.youtube.com/embed/eIrMbAQSU34?start="+start+"&end="+end;
-
 }
