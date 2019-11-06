19998f055d1f3fc5508c3e77de52a6c179f1d3d5
sreeharipullasetty <33320657+sreeharipullasetty@users.noreply.github.com>
2019-11-06 13:14:21 +0530

    Add files via upload

--- src/main/webapp/scripts.js
+++ src/main/webapp/scripts.js
@@ -2,3 +2,15 @@ function javaContent(start, end, subtopic){
 document.getElementById('subTopic').innerHTML = subtopic; 	
 document.getElementById('link').src = "https://www.youtube.com/embed/eIrMbAQSU34?start="+start+"&end="+end;
 }
+function pythonContent(start, end, subtopic){
+document.getElementById('subTopic').innerHTML = subtopic; 	
+document.getElementById('link').src = "https://www.youtube.com/embed/_uQrJ0TkZlc?start="+start+"&end="+end;
+}
+function nodeContent(start, end, subtopic){
+document.getElementById('subTopic').innerHTML = subtopic; 	
+document.getElementById('link').src = "https://www.youtube.com/embed/TlB_eWDSMt4?start="+start+"&end="+end;
+}
+function sqlContent(start, end, subtopic){
+document.getElementById('subTopic').innerHTML = subtopic; 	
+document.getElementById('link').src = "https://www.youtube.com/embed/7S_tz1z_5bA?start="+start+"&end="+end;
+}
