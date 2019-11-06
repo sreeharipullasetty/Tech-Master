0b24935d9c89c5de7b811bf963a1add2fa46f89c
sreeharipullasetty <33320657+sreeharipullasetty@users.noreply.github.com>
2019-11-06 14:24:17 +0530

    Add files via upload

--- src/main/webapp/scripts.js
+++ src/main/webapp/scripts.js
@@ -14,3 +14,7 @@ function sqlContent(start, end, subtopic){
 document.getElementById('subTopic').innerHTML = subtopic; 	
 document.getElementById('link').src = "https://www.youtube.com/embed/7S_tz1z_5bA?start="+start+"&end="+end;
 }
+function csharpContent(start, end, subtopic){
+document.getElementById('subTopic').innerHTML = subtopic; 	
+document.getElementById('link').src = "https://www.youtube.com/embed/gfkTfcpWqAY?start="+start+"&end="+end;
+}
